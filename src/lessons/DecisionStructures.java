080deefdfd3693436716388e5d975ebb0cdc4d58
e28166 <e28166@FXH-0015840124>
2024-02-08 13:57:50 -0500

    s

--- src/lessons/DecisionStructures.java
+++ src/lessons/DecisionStructures.java
@@ -4,39 +4,25 @@ public class DecisionStructures {
 
 	public static void main(String[] args) {
 
-		/* 
-		 	Comparison operators:
-			> Greater than
-			< Less than
-			>= Greater than or equal to
-			<= Less than or equal to 
-			== equal to
-			!= not equal to
-		*/
 
-		// variables
-		int num1 = 7;
-		int num2 = 13;
-		int num3 = 13;
+		/* 
+	 	Comparison operators:
+		> Greater than
+		< Less than
+		>= Greater than or equal to
+		<= Less than or equal to 
+		== equal to
+		!= not equal to
+		 */
 		
-		// > Greater than
-		if ( num1 > num3 )  {
-	
-			System.out.println(num2 + " is greater than " + num1);
-		}
-		
-		else {
+		if ( 10>1 )  {
 			
-			System.out.println(num2 + " is NOT greater than " + num1);
-		}
-		
-		// < Less than
-		if (num1 < num2)  {
-			System.out.println(num1 + " is LESS than " + num2);
+			System.out.println("true");
 		}
 		
 		else  {
-			System.out.println(num1 + " is NOT LESS than " + num2);
+			
+			System.out.println("false");
 		}
 
 	}
