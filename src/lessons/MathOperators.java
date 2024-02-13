e24cf519fa750b04fdba2cf9f225054683da260c
e28166 <e28166@FXH-0015840124>
2024-02-13 13:12:50 -0500

    s

---
+++ src/lessons/MathOperators.java
@@ -0,0 +1,40 @@
+package lessons;
+
+public class MathOperators {
+
+	public static void main(String[] args) {
+		
+		// MATH OPERATORS - +, -, *, /, %
+		
+		// variables
+		int answer1 = 0;
+		int answer2 = 0;
+		
+		// Addition:
+		System.out.println(2 + 3);
+		answer1 = 4 + 7;
+		System.out.println(answer1);
+		answer2 = answer1 + 6;
+		System.out.println(answer2);
+		
+		// Subtraction
+		System.out.println(4.3 - 2.2);
+		System.out.println(4 - 2);
+		System.out.println(4 - 2.0);
+	
+		// Multiplication
+		System.out.println(3 * 4);
+		System.out.println(3 * 4.0);
+		
+		// Division
+		System.out.println(12 / 4);
+		System.out.println(12.0 / 4);
+		
+		// Modulus - returns "remainder" of a division operation
+		System.out.println(13 % 4);
+		System.out.println(27 % 4);
+		
+		
+	}
+
+}
