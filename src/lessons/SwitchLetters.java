3ee2a171f2873770399611271e3f5f13f79c3c92
e28166 <e28166@FXH-0015840124>
2024-03-26 13:00:02 -0400

    s

---
+++ src/lessons/SwitchLetters.java
@@ -0,0 +1,37 @@
+package lessons;
+import java.util.Scanner;
+
+public class SwitchLetters {
+
+	public static void main(String[] args) {
+
+		// variables
+		Scanner sc = new Scanner(System.in);
+		String input = "";
+
+		System.out.println("Enter choice (A, B or C)");
+		input = sc.next();
+		
+		switch(input)  {
+		
+		case "A": case "a":
+			System.out.println("You entered A");
+			break;
+			
+		case "B":
+			System.out.println("You entered B");
+			break;
+			
+		case "C":
+			System.out.println("You entered C");
+			break;
+			
+		default:
+			System.out.println("You entered something else.");
+			break;
+		}
+		
+		
+	}
+
+}
